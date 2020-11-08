8e599a4c93206999d08c45845980e68e23906e10
trank <trank@DESKTOP-MCRP9DH>
2020-11-08 13:19:41 -0800

    Nam's FINAL push geez

--- SMS/src/Hello.java
+++ SMS/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("SacStateID - This line was added by Hera Flores.");
-		System.out.println("SacStateID - This line was added by Nam Tran.");
+		System.out.println("219834434 - This line was added by Nam Tran.");
 		System.out.println("219878712 - This line was added by Naba Riaz.");
 		System.out.println("SacStateID - This line was added by Misba Tabassum.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
