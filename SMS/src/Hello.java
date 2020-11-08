b8820ef832a7abaa79dd1806501596816cd67465
cpuja_000 <cpuja_000@James-Laptop.attlocal.net>
2020-11-08 14:00:24 -0800

    James's

--- SMS/src/Hello.java
+++ SMS/src/Hello.java
@@ -10,7 +10,7 @@ public class Hello {
 		System.out.println("219878712 - This line was added by Naba Riaz.");
 		System.out.println("SacStateID - This line was added by Misba Tabassum.");
 		System.out.println("SacStateID - This line was added by Andrew Flores.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("216294352 - This line was added by James Melanson.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
