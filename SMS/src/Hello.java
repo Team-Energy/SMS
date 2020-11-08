65a00f1e11873575d6c37c6db149332b1ad1ccc3
floresh <floresh@KRONOS2.ecs.csus.edu>
2020-11-08 15:19:07 -0800

    Lab03 final commit

--- SMS/src/Hello.java
+++ SMS/src/Hello.java
@@ -5,13 +5,13 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("SacStateID - This line was added by Hera Flores.");
-		System.out.println("219834434 - This line was added by Nam Tran.");
-		System.out.println("219878712 - This line was added by Naba Riaz.");
-		System.out.println("SacStateID - This line was added by Misba Tabassum.");
-		System.out.println("SacStateID - This line was added by Andrew Flores.");
-		System.out.println("216294352 - This line was added by James Melanson.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("Hera - This line was added by Hera Flores.");
+		System.out.println("Nam - This line was added by Nam Tran.");
+		System.out.println("Naba - This line was added by Naba Riaz.");
+		System.out.println("Misba - This line was added by Misba Tabassum.");
+		System.out.println("Andrew - This line was added by Andrew Flores.");
+		System.out.println("James - This line was added by James Melanson.");
+		System.out.println("Zoha - This line was added by Zoha Alvi.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 
